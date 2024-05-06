413961689676dfe4ab561445a5ac72524376ce64
Stuti Khanna ai <stutikhanna671@gmail.com>
2024-05-06 20:43:28 +0530

    Add files via upload

---
+++ stuti.py11.py
@@ -0,0 +1,12 @@
+import time
+recent_time=time.strftime('%H:%M:%S')
+recent_time=int(time.strftime('%H'))
+c= input("enter your name")
+
+if( recent_time<12):
+    print("Good Morning", c ,"it is",recent_time,"o'clock\nHave a great day")
+elif(12<=recent_time and recent_time<19):
+    print("Good Evening", c ,"it is",recent_time,"o'clock\nHave a great day")
+else:
+    print("Good Night", c ,"it is",recent_time,"o'clock\nSweet Dreams")
+   

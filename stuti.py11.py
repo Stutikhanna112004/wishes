468ee687db92ba9ae06b1c36f7995109afb3780b
Stuti Khanna ai <stutikhanna671@gmail.com>
2024-05-07 10:07:58 +0530

    Update stuti.py11.py

--- stuti.py11.py
+++ stuti.py11.py
@@ -4,9 +4,8 @@ recent_time=int(time.strftime('%H'))
 c= input("enter your name")
 
 if( recent_time<12):
-    print("Good Morning", c ,"it is",recent_time,"o'clock\nHave a great day")
+    print("Good Morning", c.capitalize(),".It is",recent_time,"o'clock\nHave a great day")
 elif(12<=recent_time and recent_time<19):
-    print("Good Evening", c ,"it is",recent_time,"o'clock\nHave a great day")
+    print("Good Evening", c.capitalize() ,".It is",recent_time,"o'clock\nHave a great day")
 else:
-    print("Good Night", c ,"it is",recent_time,"o'clock\nSweet Dreams")
-   
+    print("Good Night", c.capitalize() ,".It is",recent_time,"o'clock\nSweet Dreams")
